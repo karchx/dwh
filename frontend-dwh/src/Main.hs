dc8736eb4d2e3e6f0096d5cd019fb027d57ed3ae
kenethra.74 <kenethra.74@gmail.com>
2026-06-02 16:00:51 +0000

    feat: add ErrorAt pos in semantic

--- frontend-dwh/src/Main.hs
+++ frontend-dwh/src/Main.hs
@@ -1,25 +1,19 @@
 
-module Main (main, runDebugger) where
+module Main (main, runReplPipe) where
 
-import Eval (eval)
 import Parser (program)
 import Semantic (checkProgram)
 import Text.Megaparsec (parse, errorBundlePretty)
 import qualified Data.Text as T
 
-runDebugger :: String -> IO()
-runDebugger input = do
-    let txtInput = T.pack input
-    case parse program "" txtInput of
-        Left err -> putStrLn $ errorBundlePretty err
-        Right ast -> do
-            case checkProgram ast of
-                Left err -> do
-                    putStrLn "============ Semantic Error ============"
-                    print err
-                Right _ -> do
-                    putStrLn "============== AST PARSER =============="
-                    print ast
+runReplPipe :: String -> IO ()
+runReplPipe input = do
+    let txt = T.pack input
+    case parse program "REPL" txt of
+        Left parseErr -> putStrLn $ "Parse Error:\n" ++ errorBundlePretty parseErr
+        Right ast -> case checkProgram ast of
+            Left semErr    -> putStrLn $ "Semantic Error: " ++ show semErr
+            Right typedAst -> putStrLn $ "Success AST:\n" ++ show typedAst
 
 main :: IO ()
 main = putStrLn "Debugger.."
--- frontend-dwh/src/Semantic.hs
+++ frontend-dwh/src/Semantic.hs
@@ -2,7 +2,7 @@
 
 module Semantic 
     ( checkProgram
-    , SemanticError
+    , SemanticError(..)
     ) where
 
 import Syntax
@@ -16,51 +16,55 @@ data SemanticError = ErrorAt SourcePos Text deriving (Show, Eq)
 type Env = Map Text Type 
 type SemanticCtx a = StateT Env (Either SemanticError) a
 
-checkProgram :: Program -> Either SemanticError (Program (SourcePos, Type))
+checkProgram :: Program SourcePos -> Either SemanticError (Program (SourcePos, Type))
 checkProgram prog = evalStateT (mapM checkStmt prog) Map.empty
 
-checkStmt :: Stmt -> SemanticCtx (Stmt (SourcePos, Type))
+checkStmt :: Stmt SourcePos -> SemanticCtx (Stmt (SourcePos, Type))
 checkStmt (SVar pos varName expr) = do
     -- check before expr
     -- [ERROR]: var x = x + 1
     tExpr <- checkExpr expr
     modify (Map.insert varName (getType tExpr))
-    return $ SVar (pos, getType, tExpr) varName tExpr
+    return $ SVar (pos, getType tExpr) varName tExpr
 
-checkStmt (SExpr post expr) = do
+checkStmt (SExpr pos expr) = do
     tExpr <- checkExpr expr 
     return $ SExpr (pos, getType tExpr) tExpr
 
-checkExpr :: Expr -> SemanticCtx TypedExpr
-checkExpr expr@(EVar varName) = do
+checkExpr :: Expr SourcePos -> SemanticCtx (Expr (SourcePos, Type))
+checkExpr (EVar pos varName) = do
     env <- get
     case Map.lookup varName env of
-        Just typ -> return $ TypedExpr typ expr
-        Nothing -> lift $ Left $ "The variable '" <> varName <> "' has not been declared"
+        Just typ -> return $ EVar (pos, typ) varName
+        Nothing -> lift $ Left $ ErrorAt pos ("The variable '" <> varName <> "' has not been declared")
 
-checkExpr expr@(ELit _) = return $ TypedExpr TDouble expr
-checkExpr expr@(EString _) = return $ TypedExpr TString expr
-checkExpr expr@(EBool _) = return $ TypedExpr TBool expr
+checkExpr (ELit pos val) = return $ ELit (pos, TDouble) val
+checkExpr (EString pos val) = return $ EString (pos, TString) val
+checkExpr (EBool pos val) = return $ EBool (pos, TBool) val
 
-checkExpr expr@(EAdd e1 e2) = checkBinOp expr e1 e2
-checkExpr expr@(ESub e1 e2) = checkBinOp expr e1 e2
-checkExpr expr@(EMult e1 e2) = checkBinOp expr e1 e2
-checkExpr expr@(EDiv e1 e2) = checkBinOp expr e1 e2
-checkExpr expr@(EPow e1 e2) = checkBinOp expr e1 e2
+checkExpr (EAdd  pos e1 e2) = checkBinOp pos EAdd e1 e2
+checkExpr (ESub  pos e1 e2) = checkBinOp pos ESub e1 e2
+checkExpr (EMult pos e1 e2) = checkBinOp pos EMult e1 e2
+checkExpr (EDiv  pos e1 e2) = checkBinOp pos EDiv e1 e2
+checkExpr (EPow  pos e1 e2) = checkBinOp pos EPow e1 e2
 
-checkExpr expr@(ESqrt e) = do
-    TypedExpr t _ <- checkExpr e
-    case (t) of
-        TDouble -> return $ TypedExpr TDouble expr
-        _        -> lift $ Left "Types Error: expect Double"
+checkExpr (ESqrt pos e) = do
+    tExpr <- checkExpr e
+    case getType tExpr of
+        TDouble -> return $ ESqrt (pos, TDouble) tExpr
+        _        -> lift $ Left $ ErrorAt pos "Types Error: expect Double"
 
-checkBinOp :: Expr -> Expr -> Expr -> SemanticCtx TypedExpr
-checkBinOp parentExpr e1 e2 = do
-    TypedExpr t1 _ <- checkExpr e1
-    TypedExpr t2 _ <- checkExpr e2
-    case (t1, t2) of
-        (TDouble, TDouble) -> return $ TypedExpr TDouble parentExpr
-        _                  -> lift $ Left "Types Error: invalid combination use Double and Double"
+checkBinOp :: SourcePos 
+           -> ((SourcePos, Type) -> Expr (SourcePos, Type) -> Expr (SourcePos, Type) -> Expr (SourcePos, Type)) 
+           -> Expr SourcePos 
+           -> Expr SourcePos 
+           -> SemanticCtx (Expr (SourcePos, Type))
+checkBinOp pos constructor e1 e2 = do
+    t1 <- checkExpr e1
+    t2 <- checkExpr e2
+    case (getType t1, getType t2) of
+        (TDouble, TDouble) -> return $ constructor (pos, TDouble) t1 t2
+        _                  -> lift $ Left $ ErrorAt pos "Types Error: invalid combination use Double and Double"
 
 
 getType :: Expr (SourcePos, Type) -> Type
